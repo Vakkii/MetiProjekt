ff40314143534b51290d85cd71208daaef077e20
Lukas Bertsch <Lukas.Bertsch@student.reutlingen-university.de>
2020-02-21 12:04:50 +0100

    working afain

--- Arduino Projekts/Audiometer/Audiometer.ino
+++ Arduino Projekts/Audiometer/Audiometer.ino
@@ -5,6 +5,9 @@ Audiometer with Arduino UNO, GY-MAX4466 and  a 8x8 LED Matrix
 Author: Lukas Bertsch, Lukas.Bertsch@Student.Reutlingen-University.de
 last modified on 27.01.2020
 */
+/*Define LEDBAR constants
+ */
+const uint32_t BARLEVELS [] ={0b0000000001,0b0000000011, 0b0000000111, 0b0000001111, 0b0000011111, 0b0000111111, 0b0001111111, 0b0011111111, 0b0111111111, 0b1111111111};
 /*Define Constants*/
 const float vccADC = 5.0;/*5V max supply which the adc is using*/
 const float adcResolution = 1024;/*10 bit ADC*/
@@ -28,15 +31,13 @@ int audioSensorRight = A0;
 int matrixDIN = 2;
 int matrixCS = 3;
 int matrixCLK = 4;
-int barLeftDIN = 5;
-int barLeftData = 6;
-int barLeftCLK = 7;
-int barRightDIN = 8;
-int barRightData = 9;
-int barRightCLK = 10;
+int barLeftData = 5;
+int barLeftCLK = 6;
+int barRightData = 7;
+int barRightCLK = 8;
 LedControl lc = LedControl(matrixDIN,matrixCLK,matrixCS,1);
-Grove_LED_Bar barLeft(barLeftCLK, barLeftDIN, barLeftData, LED_BAR_10); // Clock pin, Data pin, Orientation
-Grove_LED_Bar barRight(barRightCLK, barRightDIN, barRightData, LED_BAR_10); // Clock pin, Data pin, Orientation
+Grove_LED_Bar barLeft(barLeftCLK, barLeftData, 0, LED_BAR_10); // Clock pin, Data pin, Orientation
+Grove_LED_Bar barRight(barRightCLK, barRightData,0 , LED_BAR_10); // Clock pin, Data pin, Orientation
 int i = 0;
 int k = 0;
 int countI = 0;
@@ -47,44 +48,43 @@ void setup() {
   /*Setup Audio Input Sensors*/
   pinMode(audioSensorLeft, INPUT);
   pinMode(audioSensorRight, INPUT);
-  /*Threshold calc(threshold is always vcc/2)*/
-  threshold = (vccSoundSupply/(vccADC/adcResolution))/2;
   /*Setup LED matrix*/ 
   lc.shutdown(0,false);
   // Set brightness to a medium value
   lc.setIntensity(0,8);
   // Clear the display
   lc.clearDisplay(0);
-  barLeft.begin();
-  barRight.begin();
-  barLeft.setBits(0x0);
-  barRight.setBits(0x0);
+  //barLeft.begin();
+  //barRight.begin();
+  //barLeft.setBits(0x0);
+  //barRight.setBits(0x0);
+  threshold = (vccSoundSupply/(vccADC/adcResolution))/2;
 }
 void loop() {
   sensorValueLeft = analogRead(audioSensorLeft);
   sensorValueRight = analogRead(audioSensorRight);
   absValueLeft = abs(sensorValueLeft - threshold);
-  absValueRight = abs(threshold - sensorValueRight);
+  absValueRight = abs(sensorValueRight - threshold);
   matrixLevelLeft = map(absValueLeft, 0, highestADCValue, 0, ledCount);
   matrixLevelRight = map(absValueRight, 0, highestADCValue, 0, ledCount);
-  barLevelLeft = map(absValueLeft, 0, highestADCValue, 0, barLedCount);
-  barLevelRight = map(absValueRight, 0, highestADCValue, 0, barLedCount);
-  //Serial.print(threshold);
+  barLevelLeft = map(absValueLeft, 0, highestADCValue, 0, barLedCount-1);
+  barLevelRight = map(absValueRight, 0, highestADCValue, 0, barLedCount-1);
+  Serial.println(matrixLevelLeft);
   //Serial.print(" ");
   //Serial.print(sensorValueRight);
   //Serial.print(" ");
-  Serial.println(absValueRight);
+  //Serial.println(absValueRight);
   //Serial.println(ledLevelRight);
   setMatrixLevel(matrixLevelLeft, matrixLevelRight);
-  setMatrixLevel(barLevelLeft, barLevelRight);
+  //setMatrixLevel(barLevelLeft, barLevelRight);
+}
+void setBarLevel(int levelLeft, int levelRight){
+ 
+  barLeft.setBits(BARLEVELS[levelLeft]);
+  barRight.setBits(BARLEVELS[levelRight]);
   
 }
 void setMatrixLevel(int levelLeft, int levelRight){
-  barLevelLeft.setBits((2^levelLeft)-1);
-  barLevelRight.setBits((2^levelRight)-1);
-  
-}
-void setLMatrixLevel(int levelLeft, int levelRight){
    lc.clearDisplay(0);
   /*Row 0-3 belong to Right Sensor, Row 4-7 belong to Left Sensor*/
   if (levelRight >= 1){
